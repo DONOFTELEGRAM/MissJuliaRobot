1fd073982ddb2da7078da0b5e90e399698b159cf
Restyled.io <commits@restyled.io>
2020-10-28 07:10:39 +0000

    Restyled by isort

--- julia/modules/welcome.py
+++ julia/modules/welcome.py
@@ -3,38 +3,25 @@ import random
 import re
 import time
 from functools import partial
-from julia import LOGGER, OWNER_ID, dispatcher
+
+from telegram import (ChatPermissions, InlineKeyboardButton,
+                      InlineKeyboardMarkup, ParseMode, Update)
+from telegram.error import BadRequest
+from telegram.ext import (CallbackContext, CallbackQueryHandler,
+                          CommandHandler, Filters, MessageHandler, run_async)
+from telegram.utils.helpers import (escape_markdown, mention_html,
+                                    mention_markdown)
 
 import julia.modules.sql.welcome_sql as sql
-from julia.modules.helper_funcs.chat_status import (
-    is_user_ban_protected,
-    user_can_change,
-)
+from julia import LOGGER, OWNER_ID, dispatcher
+from julia.modules.helper_funcs.chat_status import (is_user_ban_protected,
+                                                    user_can_change)
 from julia.modules.helper_funcs.misc import build_keyboard, revert_buttons
 from julia.modules.helper_funcs.msg_types import get_welcome_type
 from julia.modules.helper_funcs.string_handling import (
-    escape_invalid_curly_brackets,
-    markdown_parser,
-)
+    escape_invalid_curly_brackets, markdown_parser)
 from julia.modules.log_channel import loggable
 from julia.modules.sql.global_bans_sql import is_user_gbanned
-from telegram import (
-    ChatPermissions,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    ParseMode,
-    Update,
-)
-from telegram.error import BadRequest
-from telegram.ext import (
-    CallbackContext,
-    CallbackQueryHandler,
-    CommandHandler,
-    Filters,
-    MessageHandler,
-    run_async,
-)
-from telegram.utils.helpers import escape_markdown, mention_html, mention_markdown
 
 VALID_WELCOME_FORMATTERS = [
     "first",

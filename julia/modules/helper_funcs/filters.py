ada4f582be63b7425fa283d89ef777155729a330
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-10-28 11:23:20 +0530

    Update filters.py

--- julia/modules/helper_funcs/filters.py
+++ julia/modules/helper_funcs/filters.py
@@ -669,39 +669,23 @@ from telegram.ext import BaseFilter
 class CustomFilters():
 
     class Supporters(BaseFilter):
-
-        @staticmethod
-        def filter(message: Message):
+        def __init__(self):
+           super(Base, self).__init__()
+           self._Base__whatever()
+        def _Base__whatever(self):
+            message = Message
             return bool(message.from_user and
                         message.from_user.id in SUPPORT_USERS)
 
     support_filter = Supporters()
 
-    class Sudoers(BaseFilter):
-
-        @staticmethod
-        def filter(message: Message):
-            return bool(message.from_user and
-                        message.from_user.id in SUDO_USERS)
-
-    sudo_filter = Sudoers()
-
-    class MimeType(BaseFilter):
-
-        def __init__(self, mimetype):
-            self.mime_type = mimetype
-            self.name = "CustomFilters.mime_type({})".format(self.mime_type)
-
-        def filter(self, message: Message):
-            return bool(message.document and
-                        message.document.mime_type == self.mime_type)
-
-    mime_type = MimeType()
 
     class HasText(BaseFilter):
-
-        @staticmethod
-        def filter(message: Message):
+        def __init__(self):
+           super(Base, self).__init__()
+           self._Base__whatever()
+        def _Base__whatever(self):
+            message = Message
             return bool(message.text or message.sticker or message.photo or
                         message.document or message.video)
 
